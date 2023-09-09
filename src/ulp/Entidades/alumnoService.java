469005030fd6a431404c5c4e3ae8e0a5c1ab8a2a
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2023-09-09 15:42:07 -0300

    Initial commit

---
+++ src/ulp/Entidades/alumnoService.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulp.Entidades;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import ulp.AccesoADatos.alumnoDAO;
+
+/**
+ *
+ * @author criss
+ */
+public class alumnoService {
+
+    private alumnoDAO dao = new alumnoDAO();
+
+    public void crearAlumno(int dni, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) throws NumberFormatException, NullPointerException, DateTimeException, Exception {
+        try {
+            alumno aux = new alumno();
+            aux.setDni(dni);
+            aux.setApellido(apellido);
+            aux.setNombre(nombre);
+            aux.setFechaNacimiento(fechaNacimiento);
+            aux.setEstado(estado);
+            dao.guardarAlumno(aux);
+        } catch (NumberFormatException a) {
+            throw new NumberFormatException("Ingrese los numero correspondientes");
+        } catch (NullPointerException b) {
+            throw new NullPointerException("No deje ninguna celda vacia");
+        } catch (DateTimeException c) {
+            throw new DateTimeException("Error al analizar la fecha");
+        } catch (Exception e) {
+            throw new Exception("Ingrese correctamente los datos");
+        }
+    }
+
+    public void eliminarAlumno(int id) throws NumberFormatException, Exception {
+        try {
+            dao.eliminarEliminarAlumno(id);
+        } catch (NumberFormatException f) {
+            throw new NumberFormatException("Ingrese correctamente el ID");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public alumno buscarAlumnoPorID(int id) throws NumberFormatException, Exception {
+        try {
+            return dao.buscarAlumnoPorID(id);
+        } catch (NumberFormatException f) {
+            throw new NumberFormatException("Ingrese el id correspondiente para buscar al alumno");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public alumno buscarAlumnoPorDNI(int dni) throws NumberFormatException, Exception {
+        try {
+            return dao.buscarAlumnoPorDNI(dni);
+        } catch (NumberFormatException f) {
+            throw new NumberFormatException("Ingrese el DNI correspondiente para buscar al alumno deseado");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public ArrayList<alumno> listarAlumno() throws Exception {
+        try {
+            return dao.listarAlumno();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void modificarAlumno(int dni, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) throws NumberFormatException, NullPointerException, DateTimeException, Exception {
+        try {
+            alumno aux = new alumno();
+            aux.setDni(dni);
+            aux.setApellido(apellido);
+            aux.setNombre(nombre);
+            aux.setFechaNacimiento(fechaNacimiento);
+            aux.setEstado(estado);
+            dao.modificarAlumno(aux);
+        } catch (NumberFormatException a) {
+            throw new NumberFormatException("Ingrese los numero correspondientes para su modificacion");
+        } catch (NullPointerException b) {
+            throw new NullPointerException("No deje ninguna celda vacia");
+        } catch (DateTimeException c) {
+            throw new DateTimeException("Error al analizar la fecha");
+        } catch (Exception e) {
+            throw new Exception("Ingrese correctamente los datos para su modificacion");
+        }
+    }
+}
---
+++ src/ulp/Entidades/materiaService.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulp.Entidades;
+
+import java.util.ArrayList;
+import ulp.AccesoADatos.materiaDAO;
+
+/**
+ *
+ * @author criss
+ */
+public class materiaService {
+
+    private materiaDAO dao = new materiaDAO();
+
+    public void guardarMateria(int idMateria, String nombre, int año, boolean estado) throws NumberFormatException, NullPointerException, Exception {
+        try {
+            materia aux = new materia();
+            aux.setIdMateria(idMateria);
+            aux.setNombre(nombre);
+            aux.setAño(año);
+            aux.setEstado(estado);
+            dao.guardarMateria(aux);
+        } catch (NumberFormatException a) {
+            throw new NumberFormatException("Ingrese el año correctamente");
+        } catch (NullPointerException b) {
+            throw new NullPointerException("Ingrese el nombre de la materia correctamente");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public materia buscarMateria(int id) throws NumberFormatException, Exception {
+        try {
+            return dao.buscarMateria(id);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Ingrese el ID correspondiente para buscar su materia");
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public void modificarMateria(int idMateria, String nombre, int año, boolean estado) throws NumberFormatException, NullPointerException, Exception {
+        try {
+            materia aux = new materia();
+            aux.setIdMateria(idMateria);
+            aux.setNombre(nombre);
+            aux.setAño(año);
+            aux.setEstado(estado);
+            dao.modificarMateria(aux);
+        } catch (NumberFormatException a) {
+            throw new NumberFormatException("Ingrese los datos numericos correctos");
+        } catch (NullPointerException b) {
+            throw new NullPointerException("Ingrese el nombre de la materia correctamente");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void eliminarMateria(int id) throws NumberFormatException, Exception {
+        try {
+            dao.eliminarMateria(id);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Introduce el ID correctamente para eliminar la materia deseada");
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public ArrayList<materia> listarMateria() throws Exception {
+        try {
+            return dao.listarMateria();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+}
